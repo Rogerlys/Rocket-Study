c484169dd1eab496fbe35348b089e86a61fef0a9
chewwh09 <weishawtyhao9@gmail.com>
2020-07-16 21:19:20 +0800

    Added delete function on completed Task

--- lib/screens/CompletedTaskScreen.dart
+++ lib/screens/CompletedTaskScreen.dart
@@ -20,13 +20,21 @@ class CompletedTaskScreen extends StatefulWidget {
 class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
   var _isInit = true;
 
+  void _deleteAll() {
+    final url = 'https://todo-7b300.firebaseio.com/completedTask.json';
+    http.delete(url);
+    setState(() {
+      widget.completedTask.clear();
+    });
+  }
+
   Future<void> fetchAndCompletedTask() async {
     const url = 'https://todo-7b300.firebaseio.com/completedTask.json';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
       final List<ToDo> loadedCompletedTask = [];
-      
+
       if (extractedData != null) {
         extractedData.forEach((id, toDoData) {
           loadedCompletedTask.add(ToDo(
@@ -59,6 +67,30 @@ class _CompletedTaskScreenState extends State<CompletedTaskScreen> {
         backgroundColor: Colors.blueGrey[200],
         appBar: AppBar(
             backgroundColor: Colors.grey[200],
+            actions: <Widget>[
+              IconButton(
+                  color: Colors.red,
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                              title: Text('Are you sure?'),
+                              content: Text(
+                                  'This action will delete the entire completed task. Do you still want to proceed?'),
+                              actions: <Widget>[
+                                FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(ctx).pop();
+                                    },
+                                    child: Text('No')),
+                                FlatButton(
+                                    onPressed: () {
+                                      _deleteAll();
+                                      Navigator.of(ctx).pop();
+                                    },
+                                    child: Text('Yes'))
+                              ]))),
+            ],
             title: Text('Completed Tasks',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))),
         body: ListView(
